88c140283b9aa95a84399a29cb38f4543686d94d
branpap <brandonpapineau254@gmail.com>
2022-05-16 13:49:13 -0700

    finished replication stims

--- experiment/stimuli/itkonen-stims.js
+++ experiment/stimuli/itkonen-stims.js
@@ -88,5 +88,135 @@ var all_stims = [
             "harvinainen erikoistapaus",
             "harvinaisen erikoistapauksen"
         ]
+    },
+    {
+        "item": "valtio",
+        "type": "existential",
+        "voice": "passive",
+        "sentence": "Kirjoituksessa v\u00e4itettiin TVH:sta tulleen ...",
+        "options": [
+            "valtio valtiossa",
+            "valtion valtiossa"
+        ]
+    },
+    {
+        "item": "persikka",
+        "type": "existential",
+        "voice": "passive",
+        "sentence": "Vadille kerrottiin j\u00e4\u00e4neen ...",
+        "options": [
+            "persikka",
+            "persikan"
+        ]
+    },
+    {
+        "item": "tulipalo",
+        "type": "existential",
+        "voice": "passive",
+        "sentence": "Siell\u00e4 pel\u00e4ttiin syttyneen...",
+        "options": [
+            "tulipalo",
+            "tulipalon"
+        ]
+    },
+    {
+        "item": "jokin",
+        "type": "existential",
+        "voice": "passive",
+        "sentence": "Levyiss\u00e4 toivottiin olevan ... my\u00f6s levytyshetkest\u00e4",
+        "options": [
+            "jokin maininta",
+            "jonkin maininnan"
+        ]
+    },
+    {
+        "item": "persoonallinen",
+        "type": "existential",
+        "voice": "passive",
+        "sentence": "Kirjailijoilla luultiin olevan tyyliin ...",
+        "options": [
+            "persoonallinen sudhe",
+            "persoonallisen suhteen"
+        ]
+    },
+    {
+        "item": "kappalainen",
+        "type": "predicative",
+        "voice": "active",
+        "sentence": "Min\u00e4 ainakin luulin sen miehen olevan ...",
+        "options": [
+            "kappalainen",
+            "kappalaisen"
+        ]
+    },
+    {
+        "item": "harvinainen erikoistapaus",
+        "type": "predicative",
+        "voice": "active",
+        "sentence": "H\u00e4n arveli t\u00e4m\u00e4n ilmi\u00f6n olevan ...",
+        "options": [
+            "harvinainen erikoistapaus",
+            "harvinaisen erikoistapauksen"
+        ]
+    },
+    {
+        "item": "valtio valitossa",
+        "type": "predicative",
+        "voice": "active",
+        "sentence": "Kirjoittaja v\u00e4itt\u00e4\u00e4 TVH:n olevan ...",
+        "options": [
+            "valtio valitossa",
+            "valtion valtiossa"
+        ]
+    },
+    {
+        "item": "persikka",
+        "type": "predicative",
+        "voice": "active",
+        "sentence": "H\u00e4n sanoi sen hedelm\u00e4n olevan ...",
+        "options": [
+            "persikka",
+            "persikan"
+        ]
+    },
+    {
+        "item": "kappalainen",
+        "type": "predicative",
+        "voice": "passve",
+        "sentence": "Joissakin kyliss\u00e4 luultiin sen miehen olevan ...",
+        "options": [
+            "kappalainen",
+            "kappalaisen"
+        ]
+    },
+    {
+        "item": "harvinainen erikoistapaus",
+        "type": "predicative",
+        "voice": "passive",
+        "sentence": "Yleens\u00e4 on arveltu t\u00e4m\u00e4n ilmi\u00f6n olevan ...",
+        "options": [
+            "harvinainen erikoistapaus",
+            "harvinaisen erikoistapauksen"
+        ]
+    },
+    {
+        "item": "valtio valtiossa",
+        "type": "predicative",
+        "voice": "passive",
+        "sentence": "Kirjoituksessa v\u00e4itet\u00e4\u00e4n TVH:n olevan ...",
+        "options": [
+            "valtio valtiossa",
+            "valtion valtiossa"
+        ]
+    },
+    {
+        "item": "persikka",
+        "type": "predicative",
+        "voice": "passive",
+        "sentence": "Sen hedelm\u00e4n sanottiin olevan ...",
+        "options": [
+            "persikka",
+            "persikan"
+        ]
     }
 ]
